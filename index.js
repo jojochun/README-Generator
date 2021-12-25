340a7c86efd511452c04ad03161ed3899822bc21
jochun <jkchun@protonmail.com>
2021-12-24 22:12:47 -1000

    npm install

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
-const axios = require("axios");
 const generate = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
@@ -57,10 +56,25 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generateMarkdown(data), err =>
+        err ? console.error(err) : console.log("Success!")
+    );
+};
 
 // TODO: Create a function to initialize app
-function init() { }
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((response) => {
+            // console.log(response);
+            fs.writeFile(response.title, response);
+        })
+        .catch((error) => {
+            console.log(error);
+
+        })
+}
 
 // Function call to initialize app
 init();
